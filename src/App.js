192e01d159fccfecde69fe2d3aaa5f10a61f6e2d
Snehasish-Konger <snehasish.konger.it.2020@tint.edu.in>
2023-04-22 13:34:47 +0530

    Revert "Updates"
    This reverts commit 61e27ec503e6eb4b3852699c1c3c15a65f46f665.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StackComponent from './components/StackComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <StackComponent/>
+    </div>
+  );
+}
+
+export default App;
